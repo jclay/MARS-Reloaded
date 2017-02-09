432b72ecb7560d0b604f6616c30e95d274392768
Joel Clay <ra3don92@gmail.com>
2017-02-09 15:02:28 -0500

    Initial commit. See changelog for details

---
+++ src/main/java/mars/venus/VenusButton.java
@@ -0,0 +1,9 @@
+package mars.venus;
+  import javax.swing.*;
+import javax.swing.JButton;
+public class VenusButton extends JButton{
+    public VenusButton(Action a) {
+        super(a);
+        // this.putClientProperty("JButton.buttonType", "textured");
+    }
+}
